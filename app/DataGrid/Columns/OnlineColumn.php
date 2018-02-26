9e087b70719e9eff3119833ca62118d8343fb02c
Jovan Ivanovic <jovanivanovic@einutech.com>
2018-02-26 11:47:12 +0100

    Још лаганији фиксић!

--- app/DataGrid/Columns/OnlineColumn.php
+++ app/DataGrid/Columns/OnlineColumn.php
@@ -5,4 +5,16 @@ namespace App\DataGrid\Columns;
 class OnlineColumn extends AbstractColumn
 {
     protected $type = 'online';
+
+    public function ascUrl()
+    {
+        $currentRouteName = Route::getCurrentRoute()->getName();
+        return route($currentRouteName, ['asc' => $this->identifier()]);
+    }
+
+    public function descUrl()
+    {
+        $currentRouteName = Route::getCurrentRoute()->getName();
+        return route($currentRouteName, ['desc' => $this->identifier()]);
+    }
 }
