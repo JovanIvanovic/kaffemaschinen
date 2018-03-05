492d29e220396ba48d4508b7425a85a950e7fd5a
Jovan Ivanovic <jovanivanovic@einutech.com>
2018-03-05 15:54:57 +0100

    Popup kancerino

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Database\Category;
 use App\Models\Database\Configuration;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\View;
-use App\Models\Database\Popup;
-
 
 class Controller extends BaseController
 {
@@ -25,11 +21,5 @@ class Controller extends BaseController
             $fileViewFinder = View::getFinder();
             $fileViewFinder->prependLocation($path);
         }
-
-        $popup = Popup::where('active', 1)->first();
-        View::share('popup', $popup);
-
-        $navs = Category::where('parent_id', null)->get();
-        View::share('navs', $navs);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Database\Category;
+use App\Models\Database\Popup;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +18,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        
+        $popup = Popup::where('active', 1)->first();
+        View::share('popup', $popup);
+
+        $navs = Category::where('parent_id', null)->get();
+        View::share('navs', $navs);
     }
 
     /**
