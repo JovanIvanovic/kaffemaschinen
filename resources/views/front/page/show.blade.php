6c0466754fe7926745ba568ea939aef537deb67d
Balsa <mirkovicbalsa.rs@gmail.com>
2018-03-08 13:02:45 +0100

    terms-conditions black/white

--- resources/views/front/page/show.blade.php
+++ resources/views/front/page/show.blade.php
@@ -5,13 +5,15 @@
 @endsection
 
 @section('content')
-    <div class="container">
+<main>
+    <div class="container" style="padding: 55px 0;">
         <div class="row">
             <div class="col-12">
-                <h1>{{ $page->title }}</h1>
+                <h1 style="color: #fff;">{{ $page->title }}</h1>
 
-                <p>{!!  $page->content !!}</p>
+                <p style="color: #fff;">{!!  $page->content !!}</p>
             </div>
         </div>
     </div>
+</main>
 @endsection
